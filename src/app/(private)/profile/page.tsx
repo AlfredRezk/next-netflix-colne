2e3c11bbf3520c763bfa06c538ff4505c8559d18
Alfred Rezk <73665419+AlfredRezk@users.noreply.github.com>
2024-08-30 22:50:56 -0400

    Update page.tsx

--- src/app/(private)/profile/page.tsx
+++ src/app/(private)/profile/page.tsx
@@ -5,7 +5,7 @@ export default function ProfilePage() {
   return (
     <div className='flex items-center justify-center pt-40'>
         <div className="flex flex-col">
-          <h1 className='text-3xl md:text-5xl text-white text-center'>Who's Watching?</h1>
+          <h1 className='text-3xl md:text-5xl text-white text-center'>Who is Watching?</h1>
           <ProfileList/>
         </div>
     </div>
